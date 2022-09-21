ab5547a76773f295f7398df81fc4953037764a8f
Kris <appleofeden8686@gmail.com>
2022-09-21 20:52:15 +0200

    fixed bugs added extra capabilites

--- main.py
+++ main.py
@@ -9,14 +9,32 @@ date=datetime.today().strftime('%Y-%m-%d')
 connection=sqlite3.connect('data.db')
 cursor=connection.cursor()
 
-#sqlite3 table setup
+# sqlite3 table setup
 if(len(argv)==2 and argv[1]=='-setup'):
     cursor.execute('CREATE TABLE items (link TEXT, price REAL,date TEXT)')
+    exit()
 
-def get_price(bs)->float:
-    text = bs.find('div', class_="main-price is-big")
-    text = text.get_text()[:-3]
-    text=text.replace(" ",".")
+if (len(argv) == 2 and argv[1] == '-delete'):
+    cursor.execute('DROP TABLE items ')
+    exit()
+if (len(argv) == 2 and argv[1] == '-clearstart'):
+    cursor.execute('DELETE FROM items ')
+    
+
+
+def find_text(bs):
+    if bs.find('div', class_="main-price is-big")!=None:
+        return bs.find('div', class_="main-price is-big")
+    if bs.find('div', class_="main-price price-regular is-medium") != None:
+        return bs.find('div', class_="main-price price-regular is-medium")
+
+
+def text_to_int(string)->float:
+    
+    text = string.get_text()[:-3]
+    text=' '.join(text.split()) ###space is encoded as a no breaking space
+    text=text.replace(" ","")
+    text=text[:-2]+"."+text[-2:]
     price=float(text)
     return price
 
@@ -28,7 +46,11 @@ for link in link_array:
     
     page=get(link)
     bs = BeautifulSoup(page.content, 'html.parser')
-    price = get_price(bs)
+    
+    text=find_text(bs)
+    if text==None:
+        continue
+    price=text_to_int(text)
     cursor.execute('INSERT INTO items VALUES (?,?,?)',(link,price,date))
     connection.commit()
     print(price,link)
